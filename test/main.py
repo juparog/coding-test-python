bbd331db283790ea979df13c87ebe13a931700a3
Juan Rodriguez <juangasca95@gmail.com>
2022-07-09 23:14:38 -0500

    :sparkles: Agregando salidas en archivo .txt y consola en color azul

--- test/main.py
+++ test/main.py
@@ -4,9 +4,20 @@ MAIN
 """
 import json
 import requests
+from colorama import Back, Style
 
 def run():
     r = requests.get('https://api.chucknorris.io/jokes/random')
+
+    # json file
     with open('json-joker.json', 'w') as dataFile:
         json.dump(r.json(), dataFile, indent=4)
+    
+    # text file
+    with open('joker.txt', 'w') as dataFile:
+        dataFile.write(r.json()['value'])
+
+    # print
+    print(f'{Back.BLUE}{r.json()}{Style.RESET_ALL}')
+
     return 1
